bab4e906cdfca96db3ad71246d5eb02bffa0b0b1
Pavan Patel <patelpav@bsd.k12.in.us>
2020-09-23 21:40:07 +0000

    turtleshape - 2

--- main.py
+++ main.py
@@ -1,36 +1,24 @@
 import turtle
 t = turtle.Turtle()
-t.speed(5)
+t.speed(100)
 canvas = t.getscreen()
 canvas.bgcolor('gray')
-def move(x,y):
+mycolors = ['red','orange','green','peachpuff','violet','white','blue','yellow']
+def flower(length,x,y):
   t.penup()
   t.goto(x,y)
-  t.pendown() #end of move function
-mycolors = {'red', 'orange','peachpuff', 'violet', 'white', 'blue', 'yellow'}
-move (0,0)
-def flower(length, x, y):
-  
+  t.pendown()
   for c in mycolors:
-   t.pencolor(c)
-   for i in range(4):
-    t.rt(90)
-    t.forward(length) #end of square loop
-   t.penup()
-   t.rt(60)
-   t.pendown() #color loop ends
-move(0,-100)
-flower(10)
-move(0,0)
-flower(10)
-move (-20,20)
-flower(10)
-flower(20, -100,100)
-flower(20,-100,40)
-flower(20,-100,-20)
-x= -20
-y= 120
+    t.pencolor(c)
+    for i in range(4):
+      t.rt(90)
+      t.forward(length) #end of square loop
+    t.penup()
+    t.rt(45)
+    t.pendown()  #color loop ends
+x = -120
+y = 120
 for i in range(5):
   flower(20,x,y)
   x = x + 60
-  y = y -60
+  y = y - 60
